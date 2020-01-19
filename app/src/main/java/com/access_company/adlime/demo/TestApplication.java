34161c8e7cbf2b02168e10367deb45f19e52051f
Ham-mer <2419568879@qq.com>
2020-01-19 15:45:15 +0800

    update to 1.9.28, replace tiktok with pangle

--- app/src/main/java/com/access_company/adlime/demo/TestApplication.java
+++ app/src/main/java/com/access_company/adlime/demo/TestApplication.java
@@ -3,7 +3,6 @@ package com.access_company.adlime.demo;
 import android.app.Application;
 
 import com.access_company.adlime.core.api.AdLime;
-import com.access_company.adlime.core.api.AdLimeConfiguration;
 import com.facebook.ads.AdSettings;
 
 public class TestApplication extends Application {
@@ -13,17 +12,11 @@ public class TestApplication extends Application {
 
         AdLime.getDefault().setGdprConsent(true);
         // Show Log
-//        AdLime.setTestServer("https://sdktest.adlime.net");
-        AdLime.setLogEnable(true);
+        AdLime.getDefault().setLogEnable(true);
         // Init With AppId
-        AdLimeConfiguration configuration = new AdLimeConfiguration.Builder(this)
-                .appId("d13be96e-e172-4645-b761-4827a0ae8c0c")
-                .build();
-        AdLime.getDefault().initialize(this, configuration);
+        AdLime.getDefault().init(this, "d13be96e-e172-4645-b761-4827a0ae8c0c");
 
         // Set Facebook Network TestMode
         AdSettings.setTestMode(true);
-
     }
-
 }
