320b0fbf24f315f54653b7aed4672e323b346073
Ham-mer <2419568879@qq.com>
2020-02-17 14:30:50 +0800

    update adapter version

--- app/src/main/java/com/access_company/adlime/demo/TestApplication.java
+++ app/src/main/java/com/access_company/adlime/demo/TestApplication.java
@@ -3,7 +3,6 @@ package com.access_company.adlime.demo;
 import android.app.Application;
 
 import com.access_company.adlime.core.api.AdLime;
-import com.facebook.ads.AdSettings;
 
 public class TestApplication extends Application {
     @Override
@@ -16,5 +15,7 @@ public class TestApplication extends Application {
         AdLime.getDefault().setLogEnable(true);
         // Init With AppId
         AdLime.getDefault().init(this, "d13be96e-e172-4645-b761-4827a0ae8c0c");
+        AdLime.getDefault().setNetworkDebugMode(true);
+        AdLime.getDefault().setNetworkTestMode(true);
     }
 }
